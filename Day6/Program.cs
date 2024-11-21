f5254fe0188e410a788e74f3357cd375b32f5a29
saraelshaer <saraelshaer923@gmail.com>
2024-11-21 13:18:15 -0800

    Add task1 c#

---
+++ Day6/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Day6C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name : ");
+            string name = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Enter your age : ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("--------------------------");
+            Console.WriteLine($"Hi Mr {name}, Your age is {age}");
+
+            if (age >= 18)
+            {
+                Console.WriteLine("Old enough to vote!");
+            }
+            else
+            {
+                Console.WriteLine("Not old enough to vote.");
+            }
+        }
+    }
+}
