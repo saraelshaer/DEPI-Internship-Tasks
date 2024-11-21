e2f62207c49c1d5e2884f8529ca3824be5c5d61b
saraelshaer <saraelshaer923@gmail.com>
2024-11-21 13:35:11 -0800

    Add C# task 2

---
+++ Day7/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task2_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Student student1 = new Student() {Name ="Sara" , Age= 22 , Grade= "A"};
+
+           Student student2 = new Student() { Name = "Ali", Age = 21, Grade = "B" };
+
+            student1.DisplayInfo();
+            student2.DisplayInfo();
+
+        }
+    }
+}
---
+++ Day7/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_C_
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Grade { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}, Grade: {Grade}");
+        }
+
+    }
+}
