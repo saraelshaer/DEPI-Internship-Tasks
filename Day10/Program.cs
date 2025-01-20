e5ac7a1fc1c5a8f3e30c19615b8f9fc77445222c
saraelshaer <saraelshaer923@gmail.com>
2025-01-20 23:09:21 +0200

    Add Day10

---
+++ Day10/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Welecom to the program");
+            Console.WriteLine("----------------------");
+            while (true) 
+            {
+                Console.Write("Enter a number to Add to list: ");
+                int num = int.Parse(Console.ReadLine());
+                if (num != -1) 
+                {
+                    numbers.Add(num);
+                    Console.WriteLine("Number is added  ");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            int sum = 0;
+            Console.Write("Numbers = ");
+            foreach (var item in numbers)
+            {
+                Console.Write($"{item} ");
+                sum += item;
+            }
+            Console.WriteLine($"\nSum of numbers = {sum}, Average = {sum / numbers.Count}");
+            Console.WriteLine("\nSee you later :)");
+
+            Console.ReadKey();
+        }
+    }
+}
